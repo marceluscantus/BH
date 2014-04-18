8704a77e021aab08ecb4927b9b24712b1131d630
marceluscantus <marcelcl@gmail.com>
2014-04-18 21:15:14 +0200

    Test unitarios y funcionales
    He tenido problemas con la plurarización automática de Rails he
    definido en config/initializers/inflections.rb el plural de glasses
    como glasses. Espero que no nos de problemas que sea igual.

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -1,7 +1,9 @@
 class Manufacturer < ActiveRecord::Base
   # attr_accessible :title, :body
-  validates_presence_of :company_name
+  has_many :glasses
   attr_accessible :company_name, :city, :telephone, :mail, :address
+  validates_presence_of :company_name
+
   def name
       "#{company_name}"
   end
--- test/unit/glasses_test.rb
+++ test/unit/glasses_test.rb
@@ -1,7 +1,39 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class GlassesTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :manufacturers, :glasses
+
+  test "failing_create" do
+    glasses = Glasses.new
+    assert_equal false, glasses.save
+    assert_equal 3, glasses.errors.count
+    assert glasses.errors[:glasses_name]
+    assert glasses.errors[:price]
+    assert glasses.errors[:manufacturer]
+  end
+
+  test "create" do
+    glasses = Glasses.new(
+      :glasses_name => 'RoR',
+      :price => 55.9,
+      :manufacturer_id => Manufacturer.find(1).id,
+    )
+  assert glasses.save
+  end
+
+  test "belongs_to_mapping" do
+    arnette = Manufacturer.find_by_company_name("Arnette");
+    count = arnette.glasses.count
+    glasses = Glasses.new(
+      :glasses_name => 'Radial',
+      :price => 55.5,
+      :manufacturer_id => arnette.id  
+    )
+    arnette.glasses << glasses
+    arnette.reload
+    glasses.reload
+    assert_equal count + 1, arnette.glasses.count
+    assert_equal 'Arnette', glasses.manufacturer.name
+  end
+
 end
