484b8f5539e6eb46a5ce39131dc8ebebeebd700c
marceluscantus <marcelcl@gmail.com>
2014-04-18 17:35:06 +0200

    CRUD Gafas
    Controlador de gafas y vistas para crud completo

--- app/controllers/admin/glasses_controller.rb
+++ app/controllers/admin/glasses_controller.rb
@@ -1,19 +1,45 @@
 class Admin::GlassesController < ApplicationController
   def new
+    load_data
     @glasses    = Glasses.new
     @page_title = 'Crear nuevas Gafas'
   end
 
   def create
+    @glasses = Glasses.new(params[:glasses])
+    if @glasses.save
+      flash[:notice] = "Las Gafas #{@glasses.name} han sido creadas con exito."
+      redirect_to :action => 'index'
+    else
+      load_data
+      @page_title = 'Crear nuevas Gafas'
+      render :action => 'new'
+    end
   end
 
   def edit
+    load_data
+    @glasses = Glasses.find(params[:id])
+    @page_title = 'Editar Gafas'
   end
 
   def update
+    @glasses = Glasses.find(params[:id])
+    if @glasses.update_attributes(params[:glasses])
+      flash[:notice] = "Las Gafas #{@glasses.name} han sido actualizadas."
+      redirect_to :action => 'show', :id => @glasses
+    else
+      load_data
+      @page_title = 'Editar Gafas'
+      render :action => 'edit'
+    end
   end
 
   def destroy
+    @glasses = Glasses.find(params[:id])
+    @glasses.destroy
+    flash[:notice] = "Las Gafas #{@glasses.name} han sido eliminadas."
+    redirect_to :action => 'index'
   end
 
   def show
@@ -25,4 +51,10 @@ class Admin::GlassesController < ApplicationController
     @glasses    = Glasses.find(:all)
     @page_title = 'Listando Gafas'
   end
+  
+  private
+
+    def load_data
+      @manufacturers = Manufacturer.find(:all)
+    end
 end
