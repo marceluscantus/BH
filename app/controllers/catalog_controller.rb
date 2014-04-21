9753d7a4d3e91a6d72cf0829e56a06bcec8579c7
Diegales <diegoramjim@gmail.com>
2014-04-21 18:26:20 +0200

    Create catalog_controller.rb

---
+++ app/controllers/catalog_controller.rb
@@ -0,0 +1,19 @@
+class CatalogController < ApplicationController
+  before_filter :initialize_cart, :except => [:show, :latest]
+  before_filter :require_no_user
+  
+  def show
+    @glasses = Glasses.find(params[:id])
+    @page_title = @glasses.name
+  end
+  
+  def index
+    @glasses = Glasses.paginate :page => params[:page], :order => "glasses.id desc", :per_page => 5, :include => [:manufacturer]
+    @page_title = 'Catálogo'
+  end
+  
+  def latest
+    @glasses = Glasses.latest 5 # invoca al metodo "lastest" para sacar los cinco ultimos libros
+    @page_title = "Últimos modelos"
+  end
+end
