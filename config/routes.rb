bfc7db6d473d58339643a3a70c1124fd5c3a8982
Diegales <diegoramjim@gmail.com>
2014-04-21 19:28:32 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,9 @@ BH::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root :to => 'about#index'
+   root :to => 'catalog#index'
+   
+   match 'catalog' => 'catalog#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
