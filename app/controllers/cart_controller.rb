e07e18c77c79980adcfed62dd60f5158b8409798
jesus.gomez <jesus.gomezmoreno@alum.uca.es>
2014-06-02 19:45:31 +0200

    AJAX actualizado

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -8,11 +8,11 @@ class CartController < ApplicationController
 
     if request.xhr?
       @item = @cart.add params[:id]
-      flash.now[:cart_notice] = "Añadido <em>#{@item.glasses.name}</em>"
+      flash.now[:cart_notice] = "Añadido <em><b>#{@item.glasses.name}</b></em>".html_safe
       render :action => "add_with_ajax"
     elsif request.post?
       @item = @cart.add(params[:id])
-      flash[:cart_notice] = "Added <em>#{@item.glasses.name}</em>.".html_safe
+      flash[:cart_notice] = "Añadido <em><b>#{@item.glasses.name}</b></em>.".html_safe
       redirect_to :controller => 'catalog'
     else
       render :controller => 'cart', :action => 'add', :template => 'cart/add'
@@ -25,11 +25,11 @@ class CartController < ApplicationController
 
     if request.xhr?
       @item = @cart.remove params[:id]
-      flash.now[:cart_notice] = "Eliminado <em>#{@item.glasses.name}</em>"
+      flash.now[:cart_notice] = "Eliminado <em><b>#{@item.glasses.name}</b></em>".html_safe
       render :action => "remove_with_ajax"
     elsif request.post?
       @item = @cart.remove params[:id]
-      flash[:cart_notice] = "Removed 1 <em>#{@item.glasses.name}</em>.".html_safe
+      flash[:cart_notice] = "Eliminado <em><b>#{@item.glasses.name}</em></b>".html_safe
       redirect_to :controller => 'catalog'
     else
       render :controller => 'cart', :action => 'remove', :template => 'cart/remove'
@@ -42,10 +42,10 @@ class CartController < ApplicationController
     if request.xhr?
       @cart.cart_items.destroy_all
       flash.now[:cart_notice] = "Carro vaciado"
-      #render :action => "clear_with_ajax"
+      render :action => "clear_with_ajax"
     elsif request.post?
       @cart.cart_items.destroy_all
-      flash[:cart_notice] = "Cleared cart."
+      flash[:cart_notice] = "Carrito vaciado"
       redirect_to :controller => 'catalog'
     else
       render :controller => 'cart', :action => 'clear', :template => 'cart/clear'
